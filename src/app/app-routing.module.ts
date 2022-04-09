5dd8a66804130bf2209dd06d5a3dd198a402d66b
Andres Herrera <lherrera>
2022-04-09 08:34:07 -0500

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'user/login' },
+  { path: '**', pathMatch: 'full', redirectTo: 'user/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ArtistaComponent } from './artista/artista.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { ChatComponent } from './chat/chat.component';
+import { MainComponent } from './main/main.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ArtistaComponent,
+    BuscarComponent,
+    ChatComponent,
+    MainComponent,
+    TarjetasComponent,
   ],
   imports: [
     CommonModule,
