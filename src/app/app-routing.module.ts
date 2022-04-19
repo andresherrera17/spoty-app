1da3ce56f3d148219629f7786fc0038daa31a821
Andres Herrera <lherrera>
2022-04-18 20:15:23 -0500

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
-    path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
+    path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule), canActivate: [AuthGuard]
   },
   { path: '', pathMatch: 'full', redirectTo: 'user/login' },
   { path: '**', pathMatch: 'full', redirectTo: 'user/login' }
