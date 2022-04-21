24343f9639c55356adb68a0d73355a994a089003
Andres Herrera <lherrera>
2022-04-20 19:05:34 -0500

    service new releases

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../interfaces/user.interface';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  urlSpotify: string = environment.spotify.url;
+
+  user: any;
+
+  constructor(private http: HttpClient) { }
+
+
+  getNewReleases() {
+    return this.http.get<any[]>(`${this.urlSpotify}browse/new-releases?limit=20`)
+      .pipe(map((data: any) => data['albums'].item));
+  }
+
 }
